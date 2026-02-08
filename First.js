794d189e4c14d522769377d8fba521998615559a
Nam_Do_San <dhakedabhishek189@gmail.com>
2026-02-08 23:38:36 +0530

    First.js

---
+++ First.js
@@ -0,0 +1,53 @@
+let name;
+ console.log(name);
+ console.log("...........................");
+ 
+ fullname = "Pushpendra_Dhaked";
+console.log(fullname);
+
+console.log("...........................");
+console.log("Detail :-");
+
+            console.log("     let keyword are used ");
+let age =64;
+console.log("age = "+age);
+console.log(".............Change.............");
+    age =68;
+console.log("age = "+age);
+
+            console.log("     var keyword are used ");
+var ag =4;
+console.log("age = "+ag);
+console.log(".............Change.............");
+   var ag =5;
+console.log("age = "+ag);
+
+fa = "A=Gopal singh";
+console.log("Father name = " +fa);
+
+console.log("...But const keyword are not work Starting line of code then other part is same as let");
+   var ag =5;
+x =null;
+y = undefined;
+console.log(x+ "...." +y);
+if(x==y) console.log("Trure");
+else console.log("False");
+
+
+console.log("block  are used");
+{
+    let age =89;
+    console.log(age);
+}
+
+
+const student ={
+    fullName_key1 : "Rahul",
+    age_key2 : 19,
+    cgpi_key3 : 9.4,
+    isPass_key4 : 'ture',
+
+};
+
+console.log(student["fullName_key1"]);
+
