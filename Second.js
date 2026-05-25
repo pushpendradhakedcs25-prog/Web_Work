95a8182f023764d5fc5bedef771164e06a60896b
pushpendra dhaked <dhakedpushpendra7@gmail.com>
2026-05-25 18:35:18 +0530

    login

---
+++ Second.js
@@ -0,0 +1,20 @@
+a=5;
+b=2;
+console.log("a= ",a," & b= ",b);
+console.log("a +  b= ",a+b);
+console.log("a -  b= ",a-b);
+console.log("a *  b= ",a*b);
+console.log("a /  b= ",a/b);
+console.log("a %  b= ",a%b);
+console.log("a **  b= ",a**b);
+
+a**=4;
+console.log("a =",a);
+console.log("625==2",a==b);
+
+a=2;// int data type are used
+console.log("2==2",a==b);
+b="2"; //String data type are used
+console.log("2===2",a===b);
+
+
